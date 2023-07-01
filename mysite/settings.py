fd5cee67096897a51c2e18f89b8770f1c4e46d4c
gpcsantos <gpcsantos@gmail.com>
2023-07-01 10:41:43 -0300

    correções

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'novoapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- novoapp/views.py
+++ novoapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def index(request):
+    # print(request)
+    # return HttpResponse('<h1>HTTP RESPONSE</h1> Template')
+    return render(request, 'teste.html')
+
+def add(request):
+
+    return render(request, 'app.html')
